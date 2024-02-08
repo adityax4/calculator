6e953f9b7c935363a30226721f48786c2e49cf83
adityax4 <advensicaddy@gmail.com>
2024-02-08 11:57:08 +0530

    Add changeDisplay function

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 let firstNum
 let op
 let secondNum
+let displayValue=0
+
+const display = document.querySelector(".display")
+display.textContent=displayValue
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        if(button.textContent==="C"){
+            displayValue=0
+        }
+        else{
+            displayValue = button.textContent;
+        }
+        display.textContent = displayValue;
+    });
+});
+
 
 function operate(firstNum, op, secondNum){
     if(op=="+"){
